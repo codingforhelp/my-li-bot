64c8a6f22538b20c3ed142c63a0b5a187c353415
RPNS Nimsilu <93962127+codingforhelp@users.noreply.github.com>
2021-11-21 18:21:51 +0530

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -234,7 +234,7 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
             self.room = room
     opponent = game.black.name if game.white.name == user_profile["username"] else game.white.name
     conversation.send_reply(SendLine('player'), f'All The Best @{opponent}')
-    conversation.send_reply(SendLine('spectator'), f'Have fun until watch my game @{spectator}')
+    conversation.send_reply(SendLine('spectator'), f'Have fun until watch my game @{opponent}')
     
     variant=game.perf_name
 
